4ecdcb3d189b953a92c81e72b424384ba5a221bf
Oleh Kuper <kuper.oleh.dev@gmail.com>
2024-03-16 19:23:28 +0200

    set up prisma

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,13 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 import { FaBug } from 'react-icons/fa';
+import cn from 'classnames';
 
 const NavBar = () => {
+  const currentPath = usePathname();
   const links = [
     { label: 'Dashboard', href: '/' },
     { label: 'Issues', href: '/issues' },
@@ -16,7 +21,12 @@ const NavBar = () => {
       <ul className="flex space-x-6">
         {links.map(({ label, href }) => (
           <li key={href}>
-            <Link href={href} className="text-zinc-500 hover:text-zinc-800 transition-colors">
+            <Link
+              href={href}
+              className={cn('text-zinc-500 hover:text-zinc-800 transition-colors', {
+                'text-zinc-900': currentPath === href,
+              })}
+            >
               {label}
             </Link>
           </li>
