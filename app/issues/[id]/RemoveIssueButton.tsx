5469fcd8750ea27e05a547ab38ee68df059ebcb0
Oleh Kuper <kuper.oleh.dev@gmail.com>
2024-03-18 02:35:27 +0200

    delete an issue

--- app/issues/[id]/RemoveIssueButton.tsx
+++ app/issues/[id]/RemoveIssueButton.tsx
@@ -1,47 +1,63 @@
 'use client';
 
-import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 import React from 'react';
+import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 import { MdDelete } from 'react-icons/md';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
 
 type Props = {
   id: number;
 }
 
-const RemoveIssueButton: React.FC<Props> = ({ id }) => (
-  <AlertDialog.Root>
-    <AlertDialog.Trigger>
-      <Button color="red">
-        <MdDelete />
-        Delete issue
-      </Button>
-    </AlertDialog.Trigger>
+const RemoveIssueButton: React.FC<Props> = ({ id }) => {
+  const router = useRouter();
 
-    <AlertDialog.Content style={{ maxWidth: 450 }}>
-      <AlertDialog.Title>
-        Confirm deletion
-      </AlertDialog.Title>
+  const handleClick = async () => {
+    await axios.delete(`/api/issues/${id}`);
+    router.push('/issues');
+    router.refresh();
+  };
 
-      <AlertDialog.Description size="2">
-        Are you sure you want to delete this issue?
-        This issue will no longer be accessible.
-      </AlertDialog.Description>
+  return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">
+          <MdDelete />
+          Delete issue
+        </Button>
+      </AlertDialog.Trigger>
 
-      <Flex gap="3" mt="4" justify="end">
-        <AlertDialog.Cancel>
-          <Button variant="soft" color="gray">
-            Cancel
-          </Button>
-        </AlertDialog.Cancel>
+      <AlertDialog.Content style={{ maxWidth: 450 }}>
+        <AlertDialog.Title>
+          Confirm deletion
+        </AlertDialog.Title>
 
-        <AlertDialog.Action>
-          <Button variant="solid" color="red">
-            Delete
-          </Button>
-        </AlertDialog.Action>
-      </Flex>
-    </AlertDialog.Content>
-  </AlertDialog.Root>
-);
+        <AlertDialog.Description size="2">
+          Are you sure you want to delete this issue?
+          This issue will no longer be accessible.
+        </AlertDialog.Description>
+
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+
+          <AlertDialog.Action>
+            <Button
+              variant="solid"
+              color="red"
+              onClick={handleClick}
+            >
+              Delete
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
+};
 
 export default RemoveIssueButton;
