e527079f45957886eb86a815985c923173c38736
Oleh Kuper <kuper.oleh.dev@gmail.com>
2024-03-19 04:49:10 +0200

    build the latest issues component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Pagination from './components/Pagination';
+import LatestIssues from './LatestIssues';
 
 type Props = {
   searchParams: { page: string };
 }
 
-const Home: React.FC<Props> = ({ searchParams }) => (
-  <Pagination currentPage={+searchParams || 1} itemCount={100} pageSize={10} />
+const Home: React.FC<Props> = () => (
+  <LatestIssues />
 );
 
 export default Home;
